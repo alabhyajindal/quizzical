6f047714642898395b500aabe87e57124532218c
Alabhya Jindal <alabhya10@gmail.com>
2022-04-14 21:39:19 +0530

    added decodeHtml function for removing Html encoding from api data

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,32 +1,30 @@
 import React, { useEffect, useState } from "react";
-import AnswerOption from "./components/AnswerOption";
 import Question from "./components/Question";
 
 export default function Quiz() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [quizData, setQuizData] = useState("default");
+  // const [error, setError] = useState(null);
+  // const [isLoaded, setIsLoaded] = useState(false);
+  const [quizData, setQuizData] = useState([]);
 
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=5&type=multiple")
       .then((response) => response.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setQuizData(result.results);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
+      .then((result) => {
+        setQuizData(result.results);
+      });
   }, []);
 
-  if (error) {
-    return <h1>Error: {error.message}</h1>;
-  } else if (!isLoaded) {
-    return <h1>Loading...</h1>;
-  } else {
-    return <Question ques={quizData[1].question} />;
+  function decodeHtml(html) {
+    const txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
   }
+
+  return (
+    <div>
+      {quizData.map((data) => (
+        <Question ques={decodeHtml(data.question)} />
+      ))}
+    </div>
+  );
 }
