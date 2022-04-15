3c8eb667f913ab66fd5ec31140adb95fe8193317
Alabhya Jindal <alabhya10@gmail.com>
2022-04-15 13:16:12 +0530

    rendering answers and questions through local data to avoid fetching repeatedly while testing. Order of render is incorrect, a different data structure is needed

--- src/DATA.js
+++ src/DATA.js
@@ -1,4 +1,4 @@
-[
+export const DATA = [
   {
     category: "Entertainment: Books",
     type: "multiple",
--- src/Quiz.js
+++ src/Quiz.js
@@ -2,17 +2,50 @@ import React, { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import Question from "./components/Question";
 import AnswerOption from "./components/AnswerOption";
-
+import { DATA } from "./DATA.js";
 export default function Quiz() {
   const [quizData, setQuizData] = useState([]);
 
-  useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-      .then((response) => response.json())
-      .then((result) => {
-        setQuizData(result.results);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+  //     .then((response) => response.json())
+  //     .then((result) => {
+  //       setQuizData(result.results);
+  //     });
+  // }, []);
+
+  const answers = [];
+  DATA.map((data) => {
+    answers.push(data.correct_answer);
+    data.incorrect_answers.map((ans) => {
+      answers.push(ans);
+    });
+  });
+
+  const metaAnswer = [];
+  // Divide the answers array into 5 sub arrays
+  for (let i = 0; i < 20; i += 4) {
+    metaAnswer.push(answers.slice(i, i + 4));
+  }
+
+  const questions = [];
+  DATA.map((data) => {
+    questions.push(data.question);
+  });
+
+  const questionElements = questions.map((question) => {
+    return <Question key={nanoid()} ques={decodeHtml(question)} />;
+  });
+
+  const answerElements = metaAnswer.map((answer) => {
+    return answer.map((x) => {
+      return <AnswerOption ans={x} />;
+    });
+  });
+
+  // Need to use the Fisher Yates shuffle in order to randomize the arrays. Right now the correct answer is always in the first position of the answer options
+
+  console.log(metaAnswer);
 
   function decodeHtml(html) {
     const txt = document.createElement("textarea");
@@ -21,16 +54,10 @@ export default function Quiz() {
   }
 
   return (
-    <div>
-      {quizData.map((data, index) => {
-        return (
-          <div key={nanoid()}>
-            <Question ques={decodeHtml(data.question)} />
-            <AnswerOption ans={data.correct_answer} />
-          </div>
-        );
-      })}
-    </div>
+    <>
+      {questionElements}
+      <div className="answers-cont">{answerElements}</div>
+    </>
   );
 }
 
